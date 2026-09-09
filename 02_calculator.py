5f5cade6ab759affde3e8b3b1691db6436d811d7
Muqaddis Saleem <test@example.com>
2026-09-09 11:26:24 -0700

    Add Program 2 Calculator

---
+++ 02_calculator.py
@@ -0,0 +1,16 @@
+# Simple Calculator Program
+
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+remainder = first_number % second_number
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Remainder:", remainder)
