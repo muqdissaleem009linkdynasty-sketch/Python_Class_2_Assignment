c269419b29557988bc5257617748e5f2b31ab42d
Muqaddis Saleem <test@example.com>
2026-09-09 05:31:05 -0700

    Add Program 1 Personal Information

---
+++ 01_personal_info.py
@@ -0,0 +1,13 @@
+# Personal Information Program
+
+name = "Muqaddis Saleem"
+age = 18
+College = "Government"
+Class = 12
+favorite_language = "Python"
+
+print("Name:", name)
+print("Age:", age)
+print("college:", college)
+print("class:", class)
+print("Favorite Language:", favorite_language)
