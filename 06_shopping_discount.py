040cbe1a5fee6d9c961dc4a417c7866ca866f35f
Muqaddis Saleem <test@example.com>
2026-09-10 07:05:25 -0700

    Update Program 6 for Final Challenge

--- 06_shopping_discount.py
+++ 06_shopping_discount.py
@@ -1,4 +1,7 @@
+# Final Challenge - Shopping Discount with Student Discount
+
 purchase = float(input("Enter purchase amount: "))
+student = input("Are you a student? (yes/no): ").lower()
 
 if purchase < 5000:
     discount_percentage = 0
@@ -8,6 +11,9 @@ elif purchase < 20000:
     discount_percentage = 15
 else:
     discount_percentage = 20
+
+if student == "yes":
+    discount_percentage += 5
 
 discount_amount = purchase * discount_percentage / 100
 final_amount = purchase - discount_amount
